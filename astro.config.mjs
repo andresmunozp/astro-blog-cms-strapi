82ed72e44512d6e76670ab6f9bf37021fa08bd50
andresmunozp <amunozprado@unicauca.edu.co>
2025-08-01 21:09:07 -0500

    soporte con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   env: {
     schema: {
       STRAPI_URL: envField.string({ context: 'server', access: 'public' })
     }
-  }
+  },
+
+  adapter: vercel()
 });
